81259c26830fa15813c2af35ae27b6066aa2ec32
tredwood89 <tredwood89@gmail.com>
2017-10-07 02:00:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,8 @@ function  destructivelyAppendKitten(name) {
    kittens.push ('Ralph')
   return kittens
 }
+
+function destructivelyPrependKitten(name) {
+   kittens.unshift ('Bob')
+  return kittens
+}
