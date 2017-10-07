c68010f1608b7d841fcd36de8c75ba4896fdb249
tredwood89 <tredwood89@gmail.com>
2017-10-07 03:30:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function destructivelyRemoveFirstKitten() {
 }
 
 function appendKitten(name) {
-  var name = "Broom"
-   kittens = [...kittens, name]
-  return kittens
+  var kittens = moreKittens
+   moreKittens = [...kittens, name]
+  return moreKittens
 }
