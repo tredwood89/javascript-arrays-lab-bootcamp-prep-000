cde1c9b0e2a2161cc3632f4cdf5806a9ae3e7a65
tredwood89 <tredwood89@gmail.com>
2017-10-07 02:02:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function destructivelyPrependKitten(name) {
 
 function destructivelyRemoveLastKitten() {
    kittens.pop()
-  return kittens  
+  return kittens
 }
 
 function destructivelyRemoveFirstKitten() {
